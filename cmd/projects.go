3703574bdd397672e028d1c8ecde09eb90603aa4
Bernhard Reitinger <br@reecon.at>
2018-08-10 17:33:02 +0200

    Support bulk project creation and bulk project file upload

--- cmd/projects.go
+++ cmd/projects.go
@@ -18,6 +18,7 @@ const (
 	paramLocalFile = "file"
 	paramDownload  = "dl"
 	paramFile      = "file"
+	paramFileList  = "bulk"
 )
 
 var projectsCmd = &cobra.Command{
@@ -63,6 +64,25 @@ var showCmd = &cobra.Command{
 	},
 }
 
+func getFileEntries(fileName string) []string {
+
+	if fileName == "" {
+		return make([]string, 0)
+	}
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return make([]string, 0)
+	}
+	// remove empty entries
+	var r []string
+	for _, e := range strings.Split(string(content), "\n") {
+		if e != "" {
+			r = append(r, e)
+		}
+	}
+	return r
+}
+
 var newCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Create a new project",
@@ -71,23 +91,22 @@ var newCmd = &cobra.Command{
 		name, _ := cmd.Flags().GetString(paramName)
 		fileList, _ := cmd.Flags().GetString(paramFile)
 
-		if fileList != "" {
-			content, _ := ioutil.ReadFile(fileList)
-			files := strings.Split(string(content), "\n")
+		if files := getFileEntries(fileList); len(files) > 0 {
 			for _, f := range files {
-				if f != "" {
-					fmt.Print("Creating project ", f, " ...")
-					err = rex.CreateProject(client, f)
-					console(err, "Success!")
-				}
+				fmt.Print("Creating project ", f, " ...")
+				err = rex.CreateProject(client, f)
+				console(err, "Success!")
 			}
 		} else if name == "" {
 			reader := bufio.NewReader(os.Stdin)
 			fmt.Print("Project name: ")
 			name, _ = reader.ReadString('\n')
 			name = strings.Replace(name, "\n", "", -1)
+			fmt.Print("Creating new project: ", name)
+			err = rex.CreateProject(client, name)
+			console(err, "Success!")
 		} else {
-			fmt.Println("Creating new project: ", name)
+			fmt.Print("Creating new project: ", name)
 			err = rex.CreateProject(client, name)
 			console(err, "Success!")
 		}
@@ -102,15 +121,27 @@ var uploadFileCmd = &cobra.Command{
 		projectID, _ := cmd.Flags().GetString(paramProjectID)
 		localFile, _ := cmd.Flags().GetString(paramLocalFile)
 		name := filepath.Base(localFile)
+		fileList, _ := cmd.Flags().GetString(paramFileList)
 
 		if cmd.Flag(paramName).Value.String() != "" {
 			name, _ = cmd.Flags().GetString(paramName)
 		}
 
-		r, _ := os.Open(localFile)
-		defer r.Close()
-		err = rex.UploadProjectFile(client, projectID, name, localFile, r)
-		console(err, "Success!")
+		// Bulk upload
+		if files := getFileEntries(fileList); len(files) > 0 {
+			for _, f := range files {
+				fmt.Print("Uploading file ", f, " ...")
+				r, _ := os.Open(f)
+				defer r.Close()
+				err = rex.UploadProjectFile(client, projectID, filepath.Base(f), f, r)
+				console(err, "Success!")
+			}
+		} else {
+			r, _ := os.Open(localFile)
+			defer r.Close()
+			err = rex.UploadProjectFile(client, projectID, name, localFile, r)
+			console(err, "Success!")
+		}
 	},
 }
 
@@ -127,9 +158,9 @@ func init() {
 	uploadFileCmd.Flags().StringP(paramProjectID, "", "", "ProjectID [mandatory]")
 	uploadFileCmd.Flags().StringP(paramLocalFile, "", "", "Full path of local file [mandatory]")
 	uploadFileCmd.Flags().StringP(paramName, "", "", "Name for the project file")
+	uploadFileCmd.Flags().StringP(paramFileList, "", "", "File which contains a list of files to be uploaded")
 
 	uploadFileCmd.MarkFlagRequired(paramProjectID)
-	uploadFileCmd.MarkFlagRequired(paramLocalFile)
 
 	showCmd.Flags().StringP(paramProjectID, "", "", "ProjectID [mandatory]")
 	showCmd.Flags().Int(paramDownload, -1, "Download the given project file ID")
